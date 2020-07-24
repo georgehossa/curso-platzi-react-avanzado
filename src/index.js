d73122e8d445251ec00d20e54e6ff9627fab3bda
Jorge Ossa <jorgehossa@gmail.com>
2020-07-24 09:10:09 -0500

    Config vercel and Eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hola Petgram');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './pages/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
-'use strict';
-
 const path = require('path');
-const  HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
-  context: path.resolve(__dirname),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
+    filename: 'app.bundle.js'
     // publicPath: 'pathOrUrlWhenProductionBuild'
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
     ]
   },
-  resolve: {
-  },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      inject:true,
+    })
   ]
-};
+}
